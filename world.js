d868307365119294035923abcc0c15ff9747883f
aleguillou <aurelie.leguillou@gmail.com>
2016-07-20 17:18:32 +0200

    Store informations in local storage

--- world.js
+++ world.js
@@ -1,7 +1,19 @@
-const places = [
-	{name: 'Fairview', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}},
-	{name: 'La Panacée', city: 'Montpellier', location: {lt: '43.6069957', lg: '3.87536'}},
-	{name: 'L\'Heure bleue', city: 'Montpellier', location: {lt: '43.6007398', lg: '3.8767571'}},
-	{name: 'La Mezzanine', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}},
-	{name: 'Le Café Bun', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}}
-]
+class World {
+	find(name) {
+		return this.collection().filter(place => place.name === name)
+	}
+	findCity(city) {
+		if (! city) {
+			return this.collection()
+		}
+		return this.collection().filter(place => place.city === city)
+	}
+	new(place) {
+		const places = this.collection()
+		places.push(place)
+		localStorage.setItem('places', JSON.stringify(places))
+	}
+	collection() {
+		return JSON.parse(localStorage.getItem('places'))
+	}
+}
