344f8e31cad52343d0bf52b8b35199596475f9b5
aleguillou <aurelie.leguillou@gmail.com>
2016-07-20 15:50:14 +0200

    Replace static places list with dynamic list

--- world.js
+++ world.js
@@ -1,7 +1,7 @@
 const places = [
-	{name: 'Fairview', city: 'Montpellier', location: {lt: '10', lg: '100'}},
-	{name: 'La Panacée', city: 'Montpellier'},
-	{name: 'L\'Heure bleue', city: 'Montpellier'},
-	{name: 'La Mezzanine', city: 'Montpellier'},
-	{name: 'Le Café Bun', city: 'Montpellier'}
+	{name: 'Fairview', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}},
+	{name: 'La Panacée', city: 'Montpellier', location: {lt: '43.6069957', lg: '3.87536'}},
+	{name: 'L\'Heure bleue', city: 'Montpellier', location: {lt: '43.6007398', lg: '3.8767571'}},
+	{name: 'La Mezzanine', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}},
+	{name: 'Le Café Bun', city: 'Montpellier', location: {lt: '43.6117398', lg: '3.8767571'}}
 ]
